c4e6e6d3751cba59ede256040532e24bc8418d52
vantaramedia <pvtshivaji7@gmail.com>
2026-08-03 21:37:51 +0530

    Update dashboard.js

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,1 +1,33 @@
+// ===============================
+// Search Creator Table
+// ===============================
 
+const searchInput = document.getElementById("searchCreator");
+
+if (searchInput) {
+
+searchInput.addEventListener("keyup", function () {
+
+const filter = searchInput.value.toLowerCase();
+
+const rows = document.querySelectorAll("tbody tr");
+
+rows.forEach(row => {
+
+const text = row.innerText.toLowerCase();
+
+if (text.includes(filter)) {
+
+row.style.display = "";
+
+} else {
+
+row.style.display = "none";
+
+}
+
+});
+
+});
+
+}
