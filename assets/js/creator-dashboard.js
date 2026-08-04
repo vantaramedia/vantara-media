0e0cb7f8838b67a8dd5bf9c1f748bb1f959ed913
vantaramedia <pvtshivaji7@gmail.com>
2026-08-04 21:04:27 +0530

    Update creator-dashboard.js

--- assets/js/creator-dashboard.js
+++ assets/js/creator-dashboard.js
@@ -1,1 +1,21 @@
+// ==============================
+// Vantara Media Creator Dashboard
+// ==============================
 
+// Temporary Login Session
+const creatorEmail = localStorage.getItem("creatorEmail");
+
+if (!creatorEmail) {
+
+    window.location.href = "login.html";
+
+}
+
+// Welcome Message
+document.getElementById("creatorWelcome").innerHTML =
+`Welcome back 👋 <br><small>${creatorEmail}</small>`;
+
+// Temporary Dashboard Stats
+document.getElementById("totalPages").innerText = "0";
+document.getElementById("totalApplications").innerText = "0";
+document.getElementById("activeCampaigns").innerText = "0";
